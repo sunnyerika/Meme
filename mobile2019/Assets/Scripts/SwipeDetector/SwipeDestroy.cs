0c65d7d7c9d34c111012dade2db33f1dfbb9a2da
Erika <sunny_girl16@live.com>
2019-11-06 19:20:33 -0700

    Tap&SwipeDestroy working

---
+++ mobile2019/Assets/Scripts/SwipeDetector/SwipeDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        SwipeDetector.OnSwipe += SwipeDetector_OnSwipe;
+
+    }
+
+    private void SwipeDetector_OnSwipe(SwipeData data)
+    {
+        Debug.Log("SwipeAndDestroy: " );
+        Destroy(gameObject);
+    }
+
+}
