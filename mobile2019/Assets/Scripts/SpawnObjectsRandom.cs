2cb6c7677c37d67300c60c81a5423f766933753e
Erika <sunny_girl16@live.com>
2019-11-09 18:15:50 -0700

    SpawnRandom

---
+++ mobile2019/Assets/Scripts/SpawnObjectsRandom.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectsRandom : MonoBehaviour
+{
+    public float delay;
+    // public GameObject objectToSpawn;
+    public GameObject[] objectsToSpawn;
+
+    //we could make the SpawnedObjects member public and drag it to the inspector, but we use a different option:
+    private Transform spawnParent; //get a reference in Start()
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnParent = GameObject.Find("SpawnedObjects").transform; //name of the object in the inspector
+        StartCoroutine(WaitToSpawn());//to loop
+
+    }
+
+    //coroutine for delay
+    IEnumerator WaitToSpawn()
+    {
+        //create delay
+        yield return new WaitForSeconds(delay);
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        //Instantiate(objectToSpawn, transform.position, transform.rotation);//where we want to spawn it = position of spawner
+        Instantiate(objectsToSpawn[Random.Range(0, objectsToSpawn.Length)], transform.position, transform.rotation, spawnParent);//spawn as a child gameObject of the parent - 
+        //spawned objects destroyed after used
+
+
+        StartCoroutine(WaitToSpawn());//to loop
+    }
+
+
+}
