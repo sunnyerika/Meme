cbbe002db4893345b8c3116c205a3efd6a4491c4
Erika <sunny_girl16@live.com>
2019-11-12 01:53:55 -0700

    Bird5

--- mobile2019/Assets/Scripts/Blade4.cs
+++ mobile2019/Assets/Scripts/Blade4.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Blade4 : MonoBehaviour
-{// Start is called before the first frame update
-    bool isCutting = true;
-
+{    // Start is called before the first frame update
+    bool isCutting = false;
     Rigidbody2D rb;
-
     Camera cam;
+    public GameObject bladeTrailPrefab;
+    private GameObject currentBladeTrail;
 
+    Vector2 previousPosition;
+    public float minCuttingVelocity = 0.001f;
+
+    CircleCollider2D circleCollider;
 
 
     // Start is called before the first frame update
@@ -17,6 +21,7 @@ public class Blade4 : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         cam = Camera.main;
+        circleCollider = GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
@@ -43,15 +48,38 @@ public class Blade4 : MonoBehaviour
         Vector2 newPosition = cam.ScreenToWorldPoint(Input.mousePosition);//of gameobject
         rb.position = newPosition; //the rigidbody attached ot the gameobject will be repositioned
 
+        float velocity = (newPosition - previousPosition).magnitude * Time.deltaTime;
+        if (velocity > minCuttingVelocity)
+        {
+            circleCollider.enabled = true;
+        }
+        else
+        {
+            circleCollider.enabled = false;
+        }
+        previousPosition = newPosition;
     }
 
     void StartCut()
     {
         isCutting = true;
+        currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
+        circleCollider.enabled = false;
     }
 
     void StopCut()
     {
         isCutting = false;
+        currentBladeTrail.transform.SetParent(null);
+        Destroy(currentBladeTrail, 2f);
+        circleCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Something collided");
+        Destroy(other.gameObject);
+
     }
 }
+
