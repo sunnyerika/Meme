ce95f51ce68d8575a0e59d313cd5dc8e81c4fc47
Erika <sunny_girl16@live.com>
2019-11-12 09:30:10 -0700

    Bird17 instantiate different prefabs from array

---
+++ mobile2019/Assets/Scripts/InstantiateOnTouch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateOnTouch : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] prefabs;
+
+    private int randomPrefab;
+    private Touch touch;
+    private Vector2 touchPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                randomPrefab = Random.Range(0, 2);
+                Instantiate(prefabs[randomPrefab], touchPos, Quaternion.identity);
+            }
+        }
+    }
+}
