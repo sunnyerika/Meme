ce0d4b8d28d1000fc4133f7839ee258fceefdcac
Erika <sunny_girl16@live.com>
2019-11-12 03:49:01 -0700

    Bird8 instantiate static bird

---
+++ mobile2019/Assets/Scripts/Tap1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tap1 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tweet;
+    //private Transform spawnParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // spawnParent = GameObject.Find("SpawnedTweets").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()//on tap
+    {
+        Destroy(gameObject);
+        Spawn();
+
+    }
+
+    void Spawn()
+    {
+        Instantiate(tweet, transform.position, transform.rotation);//where we want to spawn it = position of spawner
+        //Instantiate(tweet, transform.position, transform.rotation, spawnParent);//spawn as a child gameObject of the parent - 
+        //spawned objects destroyed after used
+
+
+        
+    }
+}
