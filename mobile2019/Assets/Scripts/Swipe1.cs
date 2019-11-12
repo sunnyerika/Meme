4a082e5e79076cd78252626a69c19795d43cbfa6
Erika <sunny_girl16@live.com>
2019-11-12 02:07:36 -0700

    Bird6

---
+++ mobile2019/Assets/Scripts/Swipe1.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe1 : MonoBehaviour
+{
+
+    private Vector2 startTouchPosition, endTouchPosition;
+    private Rigidbody2D rb;
+    private float jumpForce = 700f;
+    private bool jumpAllowed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwipeCheck();
+    }
+
+    private void FixedUpdate()
+    {
+        JumpIfAllowed();
+    }
+
+    private void SwipeCheck()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPosition = Input.GetTouch(0).position;
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endTouchPosition = Input.GetTouch(0).position;
+
+            if (endTouchPosition.y > startTouchPosition.y)
+            {
+                jumpAllowed = true;
+                //Destroy(gameObject);
+            }
+            if ((endTouchPosition.x < startTouchPosition.x) && transform.position.x > -1.75f)
+            {
+                //StartCoroutine(Fly("left"));
+                Debug.Log("horizontal swipe left");
+                Destroy(gameObject);
+            }
+
+            if ((endTouchPosition.x > startTouchPosition.x) && transform.position.x > 1.75f)
+            {
+                Debug.Log("horizontal swipe right");
+                //StartCoroutine(Fly("right"));
+            }
+        }
+    }
+
+    private void JumpIfAllowed()
+    {
+        if (jumpAllowed)
+        {
+            //rb.AddForce(Vector2.up * jumpForce);
+           // Destroy(gameObject);
+            jumpAllowed = false;
+        }
+    }
+}
+
