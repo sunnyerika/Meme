59685fc905cad936b0d4cdb90be72e9b3c25b282
Erika <sunny_girl16@live.com>
2019-11-12 04:26:15 -0700

    Bird9 static bird & static tweet

---
+++ mobile2019/Assets/Scripts/Tap9.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tap9 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tweet;
+    [SerializeField]
+    private GameObject bird;
+    //private Transform spawnParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // spawnParent = GameObject.Find("SpawnedTweets").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()//on tap
+    {
+         Spawn();
+         Destroy(gameObject);
+       
+
+    }
+
+    void Spawn()
+    {
+        Vector3 tweetPosition = new Vector3(1f, 2f, 0f);
+        Vector3 birdPosition = new Vector3((transform.position.x), (transform.position.y +1f));
+        //Instantiate(tweet, transform.position, transform.rotation);//where we want to spawn it = position of spawner
+        Instantiate(tweet, tweetPosition, transform.rotation);                                  //Instantiate(tweet, transform.position, transform.rotation, spawnParent);//spawn as a child gameObject of the parent - 
+                                                                                                //spawned objects destroyed after used
+                                                                                                //Instantiate(bird, transform.position, transform.rotation);
+        Instantiate(bird, birdPosition, transform.rotation);
+
+
+    }
+}
