1870bd98c0e013053362ccb5e5a8fadbee188862
Erika <sunny_girl16@live.com>
2019-11-13 15:16:45 -0700

    Scene Game1 spawn dead bird

--- mobile2019/Assets/Game/Script/MouseInput2.cs
+++ mobile2019/Assets/Game/Script/MouseInput2.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MouseInput : MonoBehaviour
+public class MouseInput2 : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject myPanel,tweetPanel;
+    public GameObject myPanel, tweetPanel, spawnPoint;
+    
     [Header("Tweets Sprites")]
     public Sprite[] tweets;
 
@@ -14,47 +15,52 @@ public class MouseInput : MonoBehaviour
     public GameObject bird, flyingBird, dropingBird;
     public Sprite deadBird;
 
+
     [SerializeField]
     private float margin;
+    private float speed;
 
     private Vector2 mousePosition, startPos;
     private int index;
-    private GameObject clickedObject, flyingBirdInstance;
+    private GameObject clickedObject, flyingBirdInstance, dropingBirdInstance;
     private Sprite image;
     private bool isLeft;
     private Vector3 flyVelocity;
+    private Vector3 direction;
+    private Vector3 velocity;
     [SerializeField]
     [Header("Other Settings")]
-    private float offset,swipeMultiplier=25f,swipeSpeed=10f;
+    private float offset, swipeMultiplier = 25f, swipeSpeed = 10f;
     void Start()
     {
         Onspawn();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-    // Detecting If input hit something
+        // Detecting If input hit something
         if (Input.GetMouseButton(0))
         {
             mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit= Physics2D.Raycast(mousePosition, Vector2.zero);
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
             if (hit.collider != null)
             {
                 if (hit.collider.gameObject.tag.Equals("Bird")) //Show tweet if its a bird
                 {
                     myPanel.SetActive(true);
                     tweetPanel.GetComponent<Image>().sprite = image;
-                    tweetPanel.GetComponent<RectTransform>().position = new Vector3 (0,tweetPanel.GetComponent<RectTransform>().position.y, tweetPanel.GetComponent<RectTransform>().position.z);
+                    tweetPanel.GetComponent<RectTransform>().position = new Vector3(0, tweetPanel.GetComponent<RectTransform>().position.y, tweetPanel.GetComponent<RectTransform>().position.z);
                     tweetPanel.SetActive(true);
                 }
                 else if (hit.collider.gameObject.name.Equals("Tweet")) //move tweet if dragged using finger
                 {
                     clickedObject = hit.collider.gameObject;
                     startPos = new Vector2(clickedObject.transform.position.x, clickedObject.transform.position.y);
-                    offset = mousePosition.x- clickedObject.GetComponent<RectTransform>().position.x;
+                    offset = mousePosition.x - clickedObject.GetComponent<RectTransform>().position.x;
                     Debug.Log(offset);
                 }
 
@@ -63,39 +69,41 @@ public class MouseInput : MonoBehaviour
             else
             {
                 //myPanel.SetActive(false);
-               // tweetPanel.SetActive(false);
+                // tweetPanel.SetActive(false);
                 Debug.Log(hit);
             }
         }
         if (Input.GetMouseButtonUp(0)) // Moving Tweet according to finger swipe and Checking for win and fail state
         {
             //Debug.Log(clickedObject);
-            if (clickedObject!=null) {
+            if (clickedObject != null)
+            {
                 Vector2 destPos = new Vector2((clickedObject.transform.position.x + (offset * swipeMultiplier)), clickedObject.transform.position.y);
-               
+                
                 clickedObject.transform.position = Vector2.Lerp(startPos, destPos, swipeSpeed * Time.deltaTime);
                 myPanel.SetActive(false);
                 if (offset > margin && index < 3) // Towards right
                 {
- 
-                   
-                        offset = 0f;
-                        bird.SetActive(false);
-                        tweetPanel.SetActive(false);
-                        FlyAway();
-                        Respawn();
+
+
+                    offset = 0f;
+                    bird.SetActive(false);
+                    tweetPanel.SetActive(false);
+                    FlyAway();
+                    Respawn();
+
                     
-                    /*else
                     {
                         offset = 0f;                       
                         tweetPanel.SetActive(false);
                         Respawn();
-                    }*/
+                        DropDown();
+                    }
 
                 }
                 else if (offset < -margin) // Towards left
                 {
-                    if (index >= 3) 
+                    if (index >= 3)
                     {
                         offset = 0f;
                         bird.SetActive(false);
@@ -103,18 +111,20 @@ public class MouseInput : MonoBehaviour
                         FlyAway();
                         Respawn();
                     }
-                    /*else
+                    else
                     {
                         bird.SetActive(false);
                         offset = 0f;
                         tweetPanel.SetActive(false);
                         Respawn();
-                    }*/
+                        DropDown();
+                    }
                 }
+                
             }
         }
 
-        if(flyingBirdInstance != null)
+        if (flyingBirdInstance != null)
         {
             FlyTimeline();
         }
@@ -154,12 +164,36 @@ public class MouseInput : MonoBehaviour
         else
         {
             flyVelocity = new Vector3(300, 100, 0);
+
         }
     }
+
+    private void DropDown()
+    {
+        Destroy(flyingBirdInstance);
+        dropingBirdInstance = Instantiate(dropingBird, spawnPoint.transform.position, spawnPoint.transform.rotation);
+        dropingBirdInstance.transform.localScale = bird.transform.localScale;
+        
+        //flyVelocity = new Vector3(0, 300, 0);
+        /*
+        speed = 2;
+        direction = Vector3.down;
+        velocity = speed * direction;*/
+        //dropingBirdInstance.GetComponent<Rigidbody>().velocity = velocity;
+       // Vector2 destPos = new Vector2(bird.transform.position.x , -800);
+
+       // dropingBirdInstance.transform.position = Vector2.Lerp(startPos, destPos, swipeSpeed * Time.deltaTime);
+        
+    }
+
 
     private void FlyTimeline()
     {
         flyingBirdInstance.transform.Translate(flyVelocity * Time.deltaTime);
     }
-  
+
+    private void FlyTimelineDrop( )
+    {
+        dropingBird.transform.Translate(flyVelocity * Time.deltaTime);
+    }
 }
